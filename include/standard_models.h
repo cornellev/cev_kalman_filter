a3d9b51ea8f0cb698de81fe47b0c4211f22ec2e0
Sidharth Rao <sidharthmrao@gmail.com>
2024-12-09 23:26:31 -0500

    Add Cartesian standard model.

--- include/standard_models.h
+++ include/standard_models.h
@@ -2,6 +2,8 @@
 
 namespace ckf {
     namespace standard_models {
+        // ACKERMANN MODEL
+
         class AckermannModel : public Model {
         private:
             double wheelbase;
@@ -10,6 +12,21 @@ namespace ckf {
         public:
             // Constructor
             AckermannModel(V state, M covariance, M process_covariance, double wheelbase, std::vector<std::string> state_mask = {"x", "y", "d_x", "d_y", "yaw", "tau"});
+
+            V update_step(double time);
+            M update_jacobian(double dt);
+            M state_matrix_multiplier();
+        };
+
+        // CARTESIAN MODEL
+
+        class CartesianModel : public Model {
+        private:
+            M multiplier = M::Zero();
+
+        public:
+            // Constructor
+            CartesianModel(V state, M covariance, M process_covariance, double wheelbase, std::vector<std::string> state_mask = {"x", "y", "d_x", "d_y", "yaw", "d_yaw"});
 
             V update_step(double time);
             M update_jacobian(double dt);
--- src/standard_models.cpp
+++ src/standard_models.cpp
@@ -3,6 +3,8 @@
 namespace ckf {
 
     namespace standard_models {
+        // ACKERMANN MODEL
+
         // Constructor definition
         AckermannModel::AckermannModel(V state, M covariance, M process_covariance,
             double wheelbase, std::vector<std::string> state_mask)
@@ -34,7 +36,7 @@ namespace ckf {
             double new_tau = tau_ + d_tau_ * dt;
 
             double new_x = x_ + d_x_ * cos(yaw_ + tau_) * dt;
-            double new_y = y_ + d_x_ * sin(yaw_ + tau_) * dt;
+            double new_y = y_ + d_y_ * sin(yaw_ + tau_) * dt;
 
             V new_state = this->state;
 
@@ -81,6 +83,83 @@ namespace ckf {
 
         // Get state matrix multiplier definition
         M AckermannModel::state_matrix_multiplier() {
+            return multiplier;
+        }
+
+        // CARTESIAN MODEL
+
+        CartesianModel::CartesianModel(V state, M covariance, M process_covariance,
+            double wheelbase, std::vector<std::string> state_mask)
+            : Model(state, covariance, process_covariance) {
+            multiplier = state_mask_to_matrix(state_mask);
+        }
+
+        // Update step definition
+        V CartesianModel::update_step(double time) {
+            // Extract state definitions
+            double x_ = state[state::x];
+            double y_ = state[state::y];
+            double d_x_ = state[state::d_x];
+            double d_y_ = state[state::d_y];
+            double d2_x_ = state[state::d2_x];
+            double d2_y_ = state[state::d2_y];
+            double yaw_ = state[state::yaw];
+            double d_yaw_ = state[state::d_yaw];
+
+            double dt = time - most_recent_update_time;
+
+            double new_yaw = yaw_ + d_yaw_ * dt;
+
+            double new_d_x = d_x_ + d2_x_ * dt;
+            double new_d_y = d_y_ + d2_y_ * dt;
+
+            double new_x = x_ + d_x_ * cos(yaw_) * dt + .5 * d2_x_ * cos(yaw_) * dt * dt;
+            double new_y = y_ + d_y_ * sin(yaw_) * dt + .5 * d2_y_ * sin(yaw_) * dt * dt;
+
+            V new_state = this->state;
+
+            new_state[state::x] = new_x;
+            new_state[state::y] = new_y;
+            new_state[state::d_x] = new_d_x;
+            new_state[state::d_y] = new_d_y;
+            new_state[state::yaw] = new_yaw;
+
+            return new_state;
+        }
+
+        // Update Jacobian definition
+        M CartesianModel::update_jacobian(double dt) {
+            // Extract state definitions
+            double x_ = state[state::x];
+            double y_ = state[state::y];
+            double d_x_ = state[state::d_x];
+            double d_y_ = state[state::d_y];
+            double d2_x_ = state[state::d2_x];
+            double d2_y_ = state[state::d2_y];
+            double yaw_ = state[state::yaw];
+            double tau_ = state[state::tau];
+
+            M F_k = MatrixXd::Identity(S, S);
+
+            F_k(state::x, state::d_x) = dt;
+            F_k(state::y, state::d_y) = dt;
+
+            F_k(state::x, state::d2_x) = .5 * dt * dt;
+            F_k(state::y, state::d2_y) = .5 * dt * dt;
+
+            F_k(state::x, state::yaw) = -dt * d_x_ * sin(yaw_);
+            F_k(state::y, state::yaw) = dt * d_y_ * cos(yaw_);
+
+            F_k(state::d_x, state::d2_x) = dt;
+            F_k(state::d_y, state::d2_y) = dt;
+
+            F_k(state::yaw, state::d_yaw) = dt;
+
+            return F_k;
+        }
+
+        // Get state matrix multiplier definition
+        M CartesianModel::state_matrix_multiplier() {
             return multiplier;
         }
     }
